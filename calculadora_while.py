03cbf1483ae60b62723794c95f281385838c88f9
Julio Pedro <profissionaljuliopedro@gmail.com>
2022-03-29 14:04:18 -0300

    praticando comando while

--- calculadora_while.py
+++ calculadora_while.py
@@ -6,10 +6,10 @@ while True:
     print()
     num_1 = input('Digite um número: ')
     num_2 = input('Digite outro número: ')
-    operador = input('Digite um operador: ')
+    operador = input('Digite um operador (+ , - , / ou *): ')
 
     if not num_1.isnumeric() or not num_2.isnumeric():
-        print('Você precisa digitar um número!')
+        print('Você precisa digitar um número nas duas entradas!')
         continue
 
     num_1 = int(num_1)
@@ -24,4 +24,4 @@ while True:
     elif operador == '*':
         print('Resultado: ', num_1 * num_2)
     else:
-        print('Operador inválido!')
+        print('Operador inválido, escolha um entre as possibilidades(+ , - , / ou *).')
